78f9d1a8223e33dac38a735d764299c96eff290d
Trang Hoang <49129207+tranghoang200@users.noreply.github.com>
2021-03-08 15:53:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+  function rate(){
+    var ratevalue=document.getElementById("interestRateate");
+    document.getElementById("r").innerHTML=ratevalue.value+"%";
+  }
+
+  function compute(){
+    var ratevalue=parseFloat(document.getElementById("interestRateate").value);
+    var principal=parseFloat(document.getElementById("amount").value);
+    var time=parseFloat(document.getElementById("nberOfYears").value);
+    var total= parseFloat(principal*(1+((ratevalue/100)*(time))));
+    if(document.getElementById("amount").value==""){
+      alert("Please enter the principal amount");
+    }
+    var today=new Date();
+    var year=today.getFullYear();
+    time+=year;
+    var sortie="If you deposit ";
+    sortie+=principal;
+    sortie+=" RWF at an interest rate of "
+    sortie+=ratevalue;
+    sortie+="%";
+    sortie+=" You will receive an amount of ";
+    sortie+=total;
+    sortie+=" RWF in the year ";
+    sortie+=time
+    document.getElementById("resultat").innerHTML=sortie;
+    }   
